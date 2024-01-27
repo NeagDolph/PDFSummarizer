adc33aa377f4fe0ffe98ecd75794b4a473f19cea
neila <neil.agarwal@shoplc.com>
2024-01-27 02:29:08 -0500

    Revert "update"
    This reverts commit 2733b93c694260cd75477c03d867e3090b5644cd.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,26 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const handleFileUpload = (event) => {
-    const file = event.target.files[0];
-    const formData = new FormData();
-    formData.append('file', file);
-
-    fetch('/upload', {
-      method: 'POST',
-      body: formData
-    })
-      .then(response => response.json())
-      .then(data => {
-        // Handle the response from the server
-        console.log(data);
-      })
-      .catch(error => {
-        // Handle any errors that occurred during the upload
-        console.error(error);
-      });
-  };
-
   return (
     <div className="App">
       <header className="App-header">
@@ -29,7 +9,6 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <input type="file" onChange={handleFileUpload} />
         <a
           className="App-link"
           href="https://reactjs.org"
